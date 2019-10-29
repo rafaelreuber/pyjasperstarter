b6706a31a0fce178ff3752232a5440c92ad66328
rafaelreuber <rafaelreuber@gmail.com>
2019-10-28 23:02:46 -0300

    Return the error message when it is not raise by jasperstarter

--- jasperstarter/util.py
+++ jasperstarter/util.py
@@ -1,9 +1,14 @@
 
 import re
 
+ptrn = re.compile('error:.*(\n.*)*')
+
 
 def get_error(stderr):
     if isinstance(stderr, bytes):
         stderr = stderr.decode()
-    msg = re.search('error:.*(\n.*)*', stderr).group()
-    return msg
+    if re.match(ptrn, stderr):
+        msg = re.search('error:.*(\n.*)*', stderr).group()
+        return msg
+    else:
+        return stderr
